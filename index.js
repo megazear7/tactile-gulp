e334714b8d84e9493f9b034484df0da2f888f993
alexlockhart7 <alex.lockhart@icfolson.com>
2017-12-12 11:44:01 -0500

    Init commit

---
+++ index.js
@@ -0,0 +1,44 @@
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+
+var runGulp = function(gulp) {
+  var sassFiles = [
+    './global/*.scss',
+    './components/**/*.scss',
+    './pages/**/*.scss'];
+
+  var jsFiles = [
+    './global/client.js',
+    './components/**/client.js',
+    './pages/**/client.js',
+    './global/client/*.js',
+    './components/**/client/*.js',
+    './pages/**/client/*.js'];
+
+  gulp.task('sass', function () {
+    return gulp.src(sassFiles)
+      .pipe(sass().on('error', sass.logError))
+      .pipe(concat('build.css'))
+      .pipe(gulp.dest('./build'));
+  });
+
+  gulp.task('sass:watch', function () {
+    gulp.watch(sassFiles, ['sass']);
+  });
+
+  gulp.task('js', function () {
+    return gulp.src(jsFiles)
+      .pipe(concat('build.js'))
+      .pipe(gulp.dest('./build'));
+  });
+
+  gulp.task('js:watch', function () {
+    gulp.watch(jsFiles, ['js']);
+  });
+
+  gulp.task('default', ['sass', 'sass:watch', 'js', 'js:watch']);
+}
+
+module.exports = {
+    runGulp: runGulp
+};
